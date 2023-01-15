7f6b278ffd5f11c20b403651de43a3ae79c9fafa
Guilherme Wustro <gui.wustro@gmail.com>
2023-01-15 03:03:18 -0300

    feat: add message error

--- src/components/TextInputLabel/index.tsx
+++ src/components/TextInputLabel/index.tsx
@@ -3,13 +3,22 @@ import { styles } from "./styles";
 
 interface IMyTextCustomFont extends TextInputProps {
 	label: string;
+	errorMessage?: string;
 }
 
-const TextInputLabel = ({ label, ...rest }: IMyTextCustomFont) => {
+const TextInputLabel = ({
+	label,
+	errorMessage,
+	...rest
+}: IMyTextCustomFont) => {
+	const hasError = !!errorMessage;
 	return (
-		<View style={styles.container}>
-			<Text style={styles.label}>{label}</Text>
-			<TextInput style={styles.input} {...rest} />
+		<View style={styles(hasError).container}>
+			<Text style={styles(hasError).label}>{label}</Text>
+			<TextInput style={styles(hasError).input} {...rest} />
+			{errorMessage && (
+				<Text style={styles(hasError).messageError}>{errorMessage}</Text>
+			)}
 		</View>
 	);
 };
--- src/components/TextInputLabel/styles.ts
+++ src/components/TextInputLabel/styles.ts
@@ -1,25 +1,31 @@
 import { StyleSheet } from "react-native";
 
-export const styles = StyleSheet.create({
-	container: {
-		width: "100%",
-		marginBottom: 10,
-	},
-	input: {
-		fontFamily: "Roboto",
-		borderColor: "#E3E1E1",
-		borderWidth: 1,
-		borderRadius: 6,
-		fontWeight: "400",
-		fontSize: 16,
-		paddingVertical: 13,
-		paddingHorizontal: 14,
-		height: 45,
-	},
-	label: {
-		fontFamily: "Raleway-SemiBold",
-		fontSize: 8,
-		marginBottom: 5,
-		color: "#000000",
-	},
-});
+export const styles = (hasError: boolean) =>
+	StyleSheet.create({
+		container: {
+			width: "100%",
+			marginBottom: 10,
+		},
+		input: {
+			fontFamily: "Roboto",
+			borderColor: hasError ? "red" : "#E3E1E1",
+			borderWidth: 1,
+			borderRadius: 6,
+			fontWeight: "400",
+			fontSize: 16,
+			paddingVertical: 13,
+			paddingHorizontal: 14,
+			height: 45,
+		},
+		label: {
+			fontFamily: "Raleway-SemiBold",
+			fontSize: 8,
+			marginBottom: 5,
+			color: hasError ? "red" : "#000000",
+		},
+		messageError: {
+			color: "red",
+			fontFamily: "Raleway-SemiBold",
+			fontSize: 8,
+		},
+	});
