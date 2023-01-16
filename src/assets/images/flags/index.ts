2779681609467ff61d1802a5342532810bd45845
Guilherme Wustro <gui.wustro@gmail.com>
2023-01-16 00:28:37 -0300

    feat: add flag images + colors

---
+++ src/assets/images/flags/index.ts
@@ -0,0 +1,8 @@
+import Amex from "./amex-flag.png";
+import DinersClub from "./diners-club-flag.png";
+import Mastercard from "./mastercard-flag.png";
+import Visa from "./visa-flag.png";
+import Elo from "./elo-flag.png";
+import Hipercard from "./hipercard-flag.png";
+
+export default { Amex, DinersClub, Hipercard, Elo, Mastercard, Visa };
---
+++ src/constants/cardsFlagInfo.ts
@@ -0,0 +1,27 @@
+import Flag from "../assets/images/flags";
+export const cardFlagInfo = {
+	elo: {
+		imageURL: Flag.Elo,
+		colors: ["#D6D6D6", "#8E8E8E"],
+	},
+	hipercard: {
+		imageURL: Flag.Hipercard,
+		colors: ["#F8D363", "#F67E32"],
+	},
+	diners: {
+		imageURL: Flag.DinersClub,
+		colors: ["#AAC701", "#328000"],
+	},
+	amex: {
+		imageURL: Flag.Amex,
+		colors: ["#5F9CA0", "#152B2D"],
+	},
+	mastercard: {
+		imageURL: Flag.Mastercard,
+		colors: ["#1488CCCC", "#2B32B2CC"],
+	},
+	visa: {
+		imageURL: Flag.Visa,
+		colors: ["#E5A6F7", "#7B4AC0"],
+	},
+};
