9c5f86b0efc92ceb53f0278d610cb7bf51a80867
Guilherme Wustro <gui.wustro@gmail.com>
2023-01-17 00:41:13 -0300

    refactor: change data store

---
+++ src/data/index.ts
@@ -0,0 +1,25 @@
+import { ICard } from "../types/Card";
+
+export const data: ICard[] = [
+	{
+		id: "9hfgheb4d-3b7d-4bad-9bdd-first",
+		card_name: "Cartão 1",
+		person_name: "Guilherme Wustro",
+		card_number: "4389 3553 3812 7633",
+		flag: "elo",
+	},
+	{
+		id: "9basdasd4d-3b7d-4bad-9bdd-second",
+		card_name: "Cartão 2",
+		person_name: "Guilherme Wustro",
+		card_number: "5153 4791 6686 6142",
+		flag: "mastercard",
+	},
+	{
+		id: "92hfghb34d-3b7d-4bad-9bdd-third",
+		card_name: "Cartão 3",
+		person_name: "Guilherme Wustro",
+		card_number: "4916 2468 1157 7513",
+		flag: "visa",
+	},
+];
--- src/redux/slicers/cardSlice.ts
+++ src/redux/slicers/cardSlice.ts
@@ -1,50 +1,6 @@
 import { ICard } from "./../../types/Card";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-
-const data: ICard[] = [
-	{
-		id: "9b1ddeb4d-3b7d-4bad-9bdd-2ads6d",
-		card_name: "Teste 1",
-		person_name: "Gyuuiytu12",
-		card_number: "1234 1234 1234 1334",
-		flag: "elo",
-	},
-	{
-		id: "9b15deb4d-3b7d-4bad-9bdd-gdfb6d",
-		card_name: "Teste 2 ",
-		person_name: "Guii ",
-		card_number: "1234 1744 1234 1334",
-		flag: "mastercard",
-	},
-	{
-		id: "92b1deb34d-3b7d-4bad-9bdd-2bdasb6d",
-		card_name: "Teste 3 ",
-		person_name: "Tesstee ",
-		card_number: "1234 1534 1234 1334",
-		flag: "amex",
-	},
-	{
-		id: "9b12deb4d-3b127d-4bad-9bdd-2b0cbvb6d",
-		card_name: "Teste 4 ",
-		person_name: "Teste 45 ",
-		card_number: "1234 1934 1234 1334",
-		flag: "hipercard",
-	},
-	{
-		id: "9b12deb4d-3b27d-4bad-9bdd-2b0cbvb6d",
-		card_name: "Teste 4 ",
-		person_name: "Teste 45 ",
-		card_number: "1234 1934 1234 1334",
-		flag: "diners",
-	},
-	{
-		id: "9b12deb4d-3b57d-4bad-9bdd-2b0cbvb6d",
-		card_name: "Teste 4 ",
-		person_name: "Teste 45 ",
-		card_number: "1234 1934 1234 1334",
-		flag: "visa",
-	},
-];
+import { data } from "../../data";
 
 const newCardData = {
 	cardList: data,
@@ -68,17 +24,21 @@ const cardSlice = createSlice({
 			const newCardList = [...state.cardList, action.payload];
 			return { ...state, cardList: newCardList };
 		},
+		changeActualId: (state, action: PayloadAction<string>) => {
+			return { ...state, actual_id: action.payload };
+		},
 		removeActualCard: (state) => {
 			const newCardList = state.cardList.filter(
 				({ id }) => id !== state.actual_id
 			);
-			return { ...state, cardList: newCardList };
+			let newCard = { ...state, cardList: newCardList };
+			if (newCardList.length > 0) {
+				newCard.actual_id = newCard.cardList[0].id;
+			}
+			return newCard;
 		},
 		toogleNumberVisibility: (state) => {
 			return { ...state, is_hidden_number: !state.is_hidden_number };
-		},
-		changeActualId: (state, action: PayloadAction<string>) => {
-			return { ...state, actual_id: action.payload };
 		},
 	},
 });
