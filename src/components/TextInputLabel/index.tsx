c385f5efff4eac87a4d289d67e56f7b8dd203acf
Guilherme Wustro <gui.wustro@gmail.com>
2023-01-16 21:36:43 -0300

    styles: add onfocus styles

--- src/components/TextInputLabel/index.tsx
+++ src/components/TextInputLabel/index.tsx
@@ -1,5 +1,6 @@
 import { View, Text, TextInputProps, TextInput } from "react-native";
 import { styles } from "./styles";
+import { useState } from "react";
 
 interface IMyTextCustomFont extends TextInputProps {
 	label: string;
@@ -11,11 +12,18 @@ const TextInputLabel = ({
 	errorMessage,
 	...rest
 }: IMyTextCustomFont) => {
+	const [isFocused, setIsFocused] = useState(false);
 	const hasError = !!errorMessage;
+
 	return (
 		<View style={styles(hasError).container}>
-			<Text style={styles(hasError).label}>{label}</Text>
-			<TextInput style={styles(hasError).input} {...rest} />
+			<Text style={[styles(hasError, isFocused).label]}>{label}</Text>
+			<TextInput
+				onFocus={() => setIsFocused(true)}
+				onBlur={() => setIsFocused(false)}
+				style={styles(hasError, isFocused).input}
+				{...rest}
+			/>
 			{errorMessage && (
 				<Text style={styles(hasError).messageError}>{errorMessage}</Text>
 			)}
--- src/components/TextInputLabel/styles.ts
+++ src/components/TextInputLabel/styles.ts
@@ -1,6 +1,6 @@
 import { StyleSheet } from "react-native";
 import { colors } from "../../themes/colors";
-export const styles = (hasError: boolean) =>
+export const styles = (hasError: boolean, isFocused = false) =>
 	StyleSheet.create({
 		container: {
 			width: "100%",
@@ -8,7 +8,11 @@ export const styles = (hasError: boolean) =>
 		},
 		input: {
 			fontFamily: "Roboto",
-			borderColor: hasError ? colors["red-500"] : colors["grey-0"],
+			borderColor: hasError
+				? colors["red-500"]
+				: isFocused
+				? colors.blue
+				: colors["grey-0"],
 			borderWidth: 1,
 			borderRadius: 6,
 			fontWeight: "400",
@@ -21,7 +25,11 @@ export const styles = (hasError: boolean) =>
 			fontFamily: "Raleway-SemiBold",
 			fontSize: 8,
 			marginBottom: 5,
-			color: hasError ? colors["red-200"] : colors.black,
+			color: hasError
+				? colors["red-200"]
+				: isFocused
+				? colors.blue
+				: colors.black,
 		},
 		messageError: {
 			color: colors["red-500"],
